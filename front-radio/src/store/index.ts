a17472f45bfbef53d90ae3069599511a65d6adf3
wilhamsJW <wilhamsmeira@gmail.com>
2024-07-28 18:46:40 -0300

    feat: remoção de states do store

--- front-radio/src/store/index.ts
+++ front-radio/src/store/index.ts
@@ -2,14 +2,10 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import registerReducer from './slices/registerSlice';
-import authenticatedUserReducer from './reducers/authenticatedUserReducer'
-import newAuthenticatedUserReducer from './reducers/newAuthenticatedUserReducer'
 
 const store = configureStore({
   reducer: {
-    register: registerReducer,
-    authenticatedUserReducer: authenticatedUserReducer,
-    newAuthenticatedUserReducer: newAuthenticatedUserReducer
+    register: registerReducer
   },
 });
 
