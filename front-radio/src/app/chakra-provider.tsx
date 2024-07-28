a3e97a5f50f479abeebcf5050239a40e4034d2fa
wilhamsJW <wilhamsmeira@gmail.com>
2024-07-28 10:53:16 -0300

    feat: Criado chakra provider para englobar aplicação e criação de modo dark ou light

---
+++ front-radio/src/app/chakra-provider.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { ChakraProvider, ColorModeProvider, CSSReset } from '@chakra-ui/react';
+import theme from '../theme/theme';
+
+const ChakraProviderWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeProvider options={{ initialColorMode: 'light', useSystemColorMode: false }}>
+        <CSSReset />
+        {children}
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
+};
+
+export default ChakraProviderWrapper;
