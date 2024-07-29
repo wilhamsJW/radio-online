2aebfcbb38fafa080cecd90f990f93c177d75cbd
wilhamsJW <wilhamsmeira@gmail.com>
2024-07-29 19:10:48 -0300

    feat: adicionado input para filtragem

---
+++ front-radio/src/components/molecules/FilterInputMolecule.tsx
@@ -0,0 +1,38 @@
+import { Input, InputGroup, InputLeftElement, useTheme, useColorMode } from '@chakra-ui/react';
+import { AiOutlineSearch } from 'react-icons/ai';
+
+interface FilterInputProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FilterInput: React.FC<FilterInputProps> = ({ value, onChange }) => {
+  const theme = useTheme();
+  const { colorMode } = useColorMode();
+  const textColor = theme.colors[colorMode].secondary;
+
+  return (
+    <InputGroup
+      w={{ base: '60%', md: '40%' }} // Ajusta a largura com base no tamanho da tela
+      ml="auto" // Garante que o InputGroup fique alinhado à direita
+    >
+      <Input
+        value={value}
+        onChange={onChange}
+        placeholder="Pesquisar por nome de rádio, país ou idioma"
+        color={textColor}
+        borderRadius="md"
+        _placeholder={{ color: textColor }}
+        _focus={{ borderColor: textColor }}
+        pr={10} 
+      />
+      <InputLeftElement
+        pointerEvents="none"
+        children={<AiOutlineSearch color={textColor} />}
+        mr={2}
+      />
+    </InputGroup>
+  );
+};
+
+export default FilterInput;
