e4f1452bafc7f589971e6d6d9eaf1921a2a5803c
wilhamsJW <wilhamsmeira@gmail.com>
2024-07-29 18:59:32 -0300

    feat: adição de cores globaisa serem usadas no lista de faboritos e estações

--- front-radio/src/theme/theme.ts
+++ front-radio/src/theme/theme.ts
@@ -1,20 +1,23 @@
-import { extendTheme, ThemeConfig, SystemStyleObject } from '@chakra-ui/react';
+import { extendTheme, SystemStyleObject } from '@chakra-ui/react';
 
 const colors = {
   light: {
     primary: '#ffffff', // Branco para o fundo claro
     secondary: '#000000', // Preto para o texto claro
-    accent: '#CCD5AE', // Cor de destaque para o modo claro (exemplo: laranja)
-    fourth: '#c2cca0;', // Nova cor adicionada para o modo claro
+    accent: '#CCD5AE', // Cor de destaque para o modo claro
+    fourth: '#c2cca0', // Cor adicional para o modo claro
+    fifth: '#CCD5AE', // Nova cor adicionada para o modo claro
   },
   dark: {
     primary: '#2F2F33', // Cor escura para o fundo
-    primarySideBar: '#ffffff', // Cor escura para o fundo
+    primarySideBar: '#ffffff', // Cor escura para o fundo da barra lateral
     secondary: '#E9EDC9', // Cor clara para o texto
-    accent: '#1E1E21', // Cor de destaque para o modo escuro (exemplo: azul)
-    fourth: '#4D4D56', // Nova cor adicionada para o modo escuro
+    accent: '#1E1E21', // Cor de destaque para o modo escuro
+    fourth: '#4D4D56', // Cor adicional para o modo escuro
+    fifth: '#62626C', // Nova cor adicionada para o modo escuro
   },
 };
+
 
 
 // Estilos globais para o tema
