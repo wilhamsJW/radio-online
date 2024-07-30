6293efc13439a3b964952f89b6dfc69ab3b02501
wilhamsJW <wilhamsmeira@gmail.com>
2024-07-30 01:04:33 -0300

    feat: criar função fetchStationsApi e atualizar interfaces
    - Criada a função etchStationsApi para buscar estações de rádio da API.
    - Atualizadas as interfaces RadioStation e StationsData para refletir a estrutura de dados retornada pela API.
    - Implementado tratamento de erros e logging na função etchStationsApi para melhor rastreamento de falhas.
    - Garantida a compatibilidade da função com o formato de resposta da API.

---
+++ front-radio/src/app/api/fetchStations.ts
@@ -0,0 +1,39 @@
+interface RadioStation {
+  name: string;
+  url: string;
+  votes: number;
+  country: string;
+  language: string;
+  countrycode: string;
+}
+
+interface StationsData {
+  stations: RadioStation[];
+  total: number;
+}
+
+export const fetchStationsApi = async (page: number, limit: number = 30): Promise<RadioStation[] | null> => {
+  try {
+    const response = await fetch(`https://de1.api.radio-browser.info/json/stations/search?limit=${limit}&page=${page}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Falha ao buscar estações');
+    }
+
+    const data = await response.json();
+    console.log('data', data);
+    
+
+    return data
+  } catch (error) {
+    console.error('Erro ao buscar estações:', error);
+    return null;
+  }
+};
+
+  
