1ae94d045431aebcb46d52d10869901eee8e96c8
wilhamsJW <wilhamsmeira@gmail.com>
2024-08-01 13:09:46 -0300

    feat: add animaçaõ com motion, é uma lib muito leve e tá sendo muito usada, isso melhora a experiência do usuário e faze ele sentir desejo de acessar o site

---
+++ front-radio/src/components/molecules/PulsingCardNoList.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useRef } from 'react';
+import { useAnimate } from 'framer-motion';
+import { Flex, Box, Text, Button, useDisclosure } from '@chakra-ui/react';
+import DrawerMobileMolecule from '../molecules/DrawerMobileMolecule';
+
+interface PulsingCardProps {
+  onOpen: () => void;
+}
+
+const PulsingCardNoList: React.FC<PulsingCardProps> = () => {
+  const [scope, animate] = useAnimate();
+  const scopeRef = useRef<HTMLDivElement>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    if (scopeRef.current) {
+      // Define a animação pulsante
+      animate(scopeRef.current, { scale: [1, 1.05, 1] }, {
+        duration: 2.5,
+        repeat: Infinity,
+        repeatType: 'loop',
+        ease: 'easeInOut'
+      });
+    }
+  }, [animate]);
+
+  return (
+    <Flex
+      justify="center"
+      align="center"
+      p={2}
+    >
+      <Box
+        ref={scopeRef}
+        
+        borderRadius="md"
+        p={6}
+
+        textAlign="center"
+        maxWidth="600px"
+        width="100%"
+        mx="auto"
+        cursor="pointer"
+        onClick={onOpen}
+      >
+        <Text fontSize="lg" mb={4} color="white">
+          Selecione uma rádio e comece a ouvir agora mesmo
+        </Text>
+        <Button
+          mt={6}
+          colorScheme="teal"
+          variant="solid"
+          size="lg"
+          _hover={{ bg: 'teal.600' }}
+          _active={{ bg: 'teal.700' }}
+          borderRadius={20}
+          onClick={onOpen}
+          display={{ base: 'inline-flex', md: 'none' }}
+        >
+          Selecionar agora
+        </Button>
+        <DrawerMobileMolecule isOpen={isOpen} onClose={onClose} />
+      </Box>
+    </Flex>
+  );
+};
+
+export default PulsingCardNoList;
