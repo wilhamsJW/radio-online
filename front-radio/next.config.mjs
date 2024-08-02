3a564968aeec3c0e6707c07d64089fdb8b7fb575
wilhamsJW <wilhamsmeira@gmail.com>
2024-08-02 16:20:37 -0300

    feat: Atualizar configuração do Next.js
    - Adicionei productionSourceMaps: true para gerar arquivos de mapa de origem em produção, facilitando a depuração do código minificado.
    - Habilitei eactStrictMode: true para detectar problemas e práticas de codificação inseguras durante o desenvolvimento, promovendo um código mais robusto.
    - Ativei swcMinify: true para minificar o código com SWC, melhorando o desempenho ao reduzir o tamanho do bundle.
    
    Essas mudanças visam melhorar a qualidade do código e a performance do aplicativo.

--- front-radio/next.config.mjs
+++ front-radio/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  productionSourceMaps: true, // Gera arquivos de mapa de origem em produção
+  reactStrictMode: true, // Ativa o modo estrito do React
+  swcMinify: true, // Ativa a minificação com SWC
+};
 
 export default nextConfig;
