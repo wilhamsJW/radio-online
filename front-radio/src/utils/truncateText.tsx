ce45da75f28c3ecea45db143c4ac5d09851ef715
wilhamsJW <wilhamsmeira@gmail.com>
2024-08-03 20:04:57 -0300

    feat: adicionado validação a mais na função pq algumas rádios vem sem nome e estou adicionando um nome padrão

--- front-radio/src/utils/truncateText.tsx
+++ front-radio/src/utils/truncateText.tsx
@@ -1,5 +1,16 @@
 const truncateText = (text: string, maxLength: number | null | undefined) => {
-    return maxLength ? text.length > maxLength ? `${text.substring(0, maxLength).trim()}...` : text.trim() : '---';
+    // Verifica se a string contém apenas espaços
+    const trimmedText = text.trim();
+    if (trimmedText.length === 0) {
+        return 'Rádio FM desconhecida';
+    }
+
+    // Trunca o texto se necessário
+    return maxLength 
+        ? trimmedText.length > maxLength 
+            ? `${trimmedText.substring(0, maxLength)}...` 
+            : trimmedText 
+        : '---';
 };
 
 export default truncateText;
