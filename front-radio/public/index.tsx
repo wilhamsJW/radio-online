94ea2a3d3fff8e45179dce1b962d725f2a8a15f8
wilhamsJW <wilhamsmeira@gmail.com>
2024-08-03 21:25:00 -0300

    chore: adição de ícone svg para botão suspenso para adicioanr música

--- front-radio/public/index.tsx
+++ front-radio/public/index.tsx
@@ -30,3 +30,7 @@ export const SkipPrevious: React.FC<IconProps> = ({ color, size = 24, ...props }
     <path d="M220-240v-480h80v480h-80Zm520 0L380-480l360-240v480Zm-80-240Zm0 90v-180l-136 90 136 90Z" />
   </svg>
 );
+
+export const PlaylistAdd: React.FC<IconProps> = ({ color, size = 24, ...props }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill={color}><path d="M120-320v-80h280v80H120Zm0-160v-80h440v80H120Zm0-160v-80h440v80H120Zm520 480v-160H480v-80h160v-160h80v160h160v80H720v160h-80Z"/></svg>
+);
