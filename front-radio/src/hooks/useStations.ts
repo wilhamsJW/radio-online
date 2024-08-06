f986a1e4378d561e1cbc72da2511dc96a005784a
wilhamsJW <wilhamsmeira@gmail.com>
2024-08-06 17:26:21 -0300

    feat: adicionado novas configurações no hook criado com reat query para buscar dados na API

--- front-radio/src/hooks/useStations.ts
+++ front-radio/src/hooks/useStations.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient  } from 'react-query';
 import { fetchStationsApi } from '../app/api/fetchStations';
 
 {/**************************************************************************************
@@ -9,15 +9,19 @@ import { fetchStationsApi } from '../app/api/fetchStations';
 
   {/** useQuery é um hook que depende do ambiente de execução do cliente, 
   pois ele faz uso de recursos específicos do cliente para gerenciar o 
-  estado de dados e o cache. */}
+  estado de dados e o cache.
+
+  ele verifica o cache do QueryClient (que foi preenchido com o dehydratedState no servidor) e, 
+  se os dados estiverem disponíveis, retorna esses dados diretamente, evitando uma nova requisição
+  de rede.*/}
 
 const useStations = (page: number) => {
+  const queryClient = useQueryClient();
   return useQuery(['stations', page], () => fetchStationsApi(page), {
     keepPreviousData: true, // Mantém dados da página anterior enquanto a nova página é carregada
-    staleTime: 60000, // Tempo em milissegundos para considerar dados como frescos
-    onError: (error) => {
-      console.error('Error fetching stations:', error);
-    }
+    staleTime: 500000, // Tempo em milissegundos para considerar dados como frescos
+    cacheTime: 500000,
+    initialData: () => queryClient.getQueryData(['stations']), // Usa dados em cache se disponíveis
   });
 };
 
